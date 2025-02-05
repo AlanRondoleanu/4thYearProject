22c341e57cd633e000a8db2e1d75038b35a86c51
Alan <megon12@gmail.com>
2025-02-05 18:59:03 +0000

    surround enemy now kinda works

--- Project1/RayCasting.h
+++ Project1/RayCasting.h
@@ -17,15 +17,10 @@ public:
 class RayCasting
 {
 public:
-    void render(sf::RenderWindow& t_window);
-
     std::vector<Unit> getBlockingUnits(Unit movingUnit, std::vector<Unit> otherUnits);
-    sf::Vector2f avoidObstacle(Unit movingUnit, std::vector<Unit> obstacleUnits, sf::Vector2f flowFieldDirection);
+    sf::Vector2f calculateAvoidanceDirection(const Unit movingUnit, const std::vector<Unit> blockingUnits);
 
 private:
-    sf::Vertex lines[2];
-
-    sf::Vector2f calculateAvoidanceDirection(Unit& movingUnit, const std::vector<Unit>& blockingUnits);
     bool rayIntersectsCircle(const sf::Vector2f& A, const sf::Vector2f& B, const sf::Vector2f& C, float totalRadius);
     sf::Vector2f normalize(const sf::Vector2f& vector);
     float dot(const sf::Vector2f& a, const sf::Vector2f& b);
